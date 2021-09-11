f7e9cf8d9ab79ce1a8d628838e38906a4bee9772
Renato Silva <reeh.cr2@gmail.com>
2021-09-11 00:53:37 -0300

    Add projeto Spring

---
+++ src/main/java/com/rsoftware/springboot/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.rsoftware.springboot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String helloMessage(){
+        return "Ola Mundo!";
+    }
+}
